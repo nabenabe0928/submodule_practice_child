09858d3992c24cf0882dc7d2d1d1e6d417f51bb7
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2021-10-09 19:43:39 +0200

    [fix] Fix setup.py to be able to include submodule

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ with open('requirements.txt', 'r') as f:
     for line in f:
         requirements.append(line.strip())
 
+requirements.append(
+    "submodule_practice_parent "
+    "@ git+ssh://git@"
+    "github.com/nabenabe0928/submodule_practice_parent"
+    "@v0.0.1#egg=submodule_practice_parent"
+)
+
 setuptools.setup(
     name="submodule_practice_child",
     version="0.0.1",
@@ -16,5 +23,6 @@ setuptools.setup(
 	python_requires='>=3.8',
     platforms=['Linux'],
     install_requires=requirements,
-    include_package_data=True
+    include_package_data=True,
+    ext_modules=[setuptools.Extension]
 )
